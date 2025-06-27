618402ff1e61f56273dab15a8147c5da0e74fa46
iijawhor <jawhorali1@gmail.com>
2025-06-27 07:23:34 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.use((req, res) => {
+  res.send("Hello from server");
+});
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
